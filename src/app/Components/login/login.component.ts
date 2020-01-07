6e156a4f1d296abbb85af706d1357c7d94bb6431
Tharuke <tharuke.lnbti@gmail.com>
2020-01-07 09:16:40 +0530

    paragraph tag css updated

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -7,6 +9,15 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
+  fname: string;
+  lname: string;
+  email: string;
+  userRole: string;
+  password: string;
+  username: string;
+  userStatus: string;
+
 
   constructor(private toastr: ToastrService) {
   }
---
+++ src/app/Model/UserObj.ts
@@ -0,0 +1,14 @@
+export class UserObj {
+
+  public fname: string;
+  public lname: string;
+  public email: string;
+  public userRole: string;
+  public password: string;
+  public username: string;
+  public userStatus: string;
+
+
+  constructor() {
+  }
+}
